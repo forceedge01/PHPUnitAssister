af994aa73d08bfa14d8f4f1fe3325739cb3846a3
Wahab Qureshi <its.inevitable@hotmail.com>
2014-11-14 12:36:52 +0000

    Pre call introduced, doesnt exit the process otherwise the same as prex

--- Src/Core/Debugger.Class.php
+++ Src/Core/Debugger.Class.php
@@ -13,13 +13,23 @@ class Debugger {
 		echo "Depreciated method used: '{$caller}', on '{$date}'";
 	}
 
-	public static function prex()
+    public static function pre()
     {
         $arguments = func_get_args();
 
         foreach($arguments as $argument)
         {
             print_r($argument);
+        }
+    }
+
+	public static function prex()
+    {
+        $args = func_get_args();
+
+        foreach($args as $arg)
+        {
+            self::pre($arg);
         }
 
         exit();
